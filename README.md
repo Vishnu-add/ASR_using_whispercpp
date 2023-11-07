2a16d335fc21e52ae1d77c42614227f691ad2399
A Vishnu Vardhan <98774143+Vishnu-add@users.noreply.github.com>
2023-11-07 17:18:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,45 +1,13 @@
-# ASR_using_whispercpp
+---
+title: Whisper CPP ASR CLI
+emoji: 🚀
+colorFrom: yellow
+colorTo: pink
+sdk: gradio
+sdk_version: 4.0.2
+app_file: app.py
+pinned: false
+license: mit
+---
 
-Run
-```
-pip install -r requirements.txt
-```
-
-## Download the whispercpp files
-- Download_link : https://github.com/ggerganov/whisper.cpp/releases/tag/v1.3.0
-- Navigate to Assests and download the desired files for your system
-  - whisper-bin-Win32.zip
-  - whisper-bin-x64.zip
-  - whisper-blas-bin-Win32.zip
-  - whisper-blas-bin-x64.zip
-
-## Download the models
-- Download_link : https://ggml.ggerganov.com/
-
-![image](https://github.com/Add-Vishnu/ASR_using_whispercpp/assets/139844342/38b2f2d1-31b4-41d1-a024-fc7d7f860335)
-
-
-## Run app.py
-
-## Errors
-- If you encounter any issue with gr.Audio uncomment the commented line and comment the existing one.
-- The existing works with gradio
-
-
-## Functions
-- transcribe: This function takes the audio input resample it using resamplr_to_16k function and saves it in  a temporary .wav file which will be deted later. The command is stored in the command variable with the main.exe path and whisper's model path.
-  ```
-  command rf"""'./whisper_blas_bin_v1_3_0/main.exe' -m './whisper_blas_bin_v1_3_0/models/ggml-model-whisper-small.en.bin' -osrt -f '{temp_audio_path}' -nt"""
-  ```
-  - -m : Used to specify the models path
-  - -osrt : Used to save the subtitles in a file
-  - -f : Used to specify the audio path to be transcribed
-  - -nt : Used to specify - no-timestamps
-- resample_to_16k: This function resamples the speech rate of audio to 16k
-
-## References:
-- Main Github : https://github.com/ggerganov/whisper.cpp
-- Files of WhisperCPP : https://github.com/ggerganov/whisper.cpp/releases/tag/v1.3.0
-- Download Models from : https://ggml.ggerganov.com/
-- Downlaod Models from Hugging face : https://huggingface.co/ggerganov/whisper.cpp
-- https://github.com/ggerganov/whisper.cpp/tree/master/models
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
--- app.py
+++ app.py
@@ -8,12 +8,34 @@ import time
 import numpy as np
 import subprocess 
 
+# command = r"""wine './whisper_blas_bin_v1_3_0/main.exe' -h"""
+# wine_command = """sudo apt-get install wine"""
+command2 = """chmod +777 ./whisper_blas_bin_v1_3_0/main.exe"""
+# wine_c = subprocess.run(wine_command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE, text=True)
+perm = subprocess.run(command2, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE, text=True)
+# result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+# print("Wine Installation: ",wine_c)
+print("Access Installation: ",perm)
+# Fpr win32 instalattion while using medium model
+# command3 = "apt install sudo"
+# command4 = "dpkg --add-architecture i386"
+# command5 = "apt-get update"
+# command6 = "apt-get install wine32:i386"
+# t1= subprocess.run(command3, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE, text=True)
+# t2= subprocess.run(command4, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE, text=True)
+# t3= subprocess.run(command5, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE, text=True)
+# t4= subprocess.run(command6, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE, text=True)
+# print("T1: ",t1)
+# print("T2: ",t2)
+# print("T3: ",t3)
+# print("T4: ",t4)
+
 
 def resample_to_16k(audio, orig_sr):
     y_resampled = librosa.resample(y=audio, orig_sr=orig_sr, target_sr = 16000)
     return y_resampled
 
-def transcribe(audio,):
+def transcribe(audio):
     sr,y = audio
     y = y.astype(np.float32)
     y /= np.max(np.abs(y))
@@ -24,7 +46,12 @@ def transcribe(audio,):
         temp_audio_path = temp_audio.name
         sf.write(temp_audio_path, y_resampled, 16000)
 
-    command = rf"""'./whisper_blas_bin_v1_3_0/main.exe' -m './whisper_blas_bin_v1_3_0/models/ggml-model-whisper-small.en.bin' -osrt -f '{temp_audio_path}' -nt"""
+
+    
+    # command = rf"""wine './whisper_blas_bin_v1_3_0/main.exe' -m './whisper_blas_bin_v1_3_0/models/ggml-model-whisper-small.en.bin' -osrt -f '{temp_audio_path}' -nt"""  # English only
+    command = rf"""wine './whisper_blas_bin_v1_3_0/main.exe' -m './whisper_blas_bin_v1_3_0/models/ggml-model-whisper-base.bin' -osrt -f '{temp_audio_path}' -nt"""    # Multilingual
+    # win32 error while using medium model
+    # command = rf"""wine './whisper_blas_bin_v1_3_0/main.exe' -m './whisper_blas_bin_v1_3_0/models/ggml-model-whisper-medium-q5_0.bin' -osrt -f '{temp_audio_path}' -nt"""    # Multilingual
     
     start_time = time.time()
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -42,9 +69,11 @@ def transcribe(audio,):
 
 demo = gr.Interface(
     transcribe,
-    gr.Audio(source="microphone"),
+    inputs = "microphone",
     # gr.Audio(sources=["microphone"]),
-    outputs=[gr.Textbox(label="CLI_Transcription"),gr.Textbox(label="Time taken for Transcription")]
+    outputs = [gr.Textbox(label="CLI_Transcription"),gr.Textbox(label="Time taken for Transcription")],
+    examples=["./Samples/Hindi_1.mp3","./Samples/Hindi_2.mp3","./Samples/Tamil_1.mp3","./Samples/Tamil_2.mp3","./Samples/Marathi_1.mp3","./Samples/Marathi_2.mp3","./Samples/Nepal_1.mp3","./Samples/Nepal_2.mp3","./Samples/Telugu_1.wav","./Samples/Telugu_2.wav","./Samples/Malayalam_1.wav","./Samples/Malayalam_2.wav","./Samples/Gujarati_1.wav","./Samples/Gujarati_2.wav","./Samples/Bengali_1.wav","./Samples/Bengali_2.wav"]
+
 )
 
 demo.launch()
